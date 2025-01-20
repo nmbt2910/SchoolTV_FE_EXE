5db9c9f511a4a2eca9d51668a288551fad35139e
Dong <taidong2k4@gmail.com>
2025-01-20 11:43:42 +0700

    Create layout header footer for page

--- SchoolTV_FE_Source/src/App.jsx
+++ SchoolTV_FE_Source/src/App.jsx
@@ -1,37 +1,26 @@
-import React from 'react';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import AllFeaturedVideo from './pages/featuredVideo/AllFeaturedVideo';
-import PlayFeaturedVideo from './pages/featuredVideo/PlayFeaturedVideo';
-import Home from './pages/Home';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AllFeaturedVideo from "./pages/featuredVideo/AllFeaturedVideo";
+import PlayFeaturedVideo from "./pages/featuredVideo/PlayFeaturedVideo";
+import Layout from "./components/layout/PageLayout";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: (
-        <>
-          <Header /> {/* Include the Header component */}
-          <Home />   {/* Include the Home component */}
-          <Footer /> {/* Include the Footer component */}
-        </>
-      ),
-    },
+      path: "",
+      element: <Layout />,
+      children: [
+        {
+          path: "featured-video",
+          element: <AllFeaturedVideo />,
+        },
 
-    {
-      path: "featured-video",
-      element: <AllFeaturedVideo />,
-    },
-
-    {
-      path: "play-featured-video",
-      element: <PlayFeaturedVideo />,
+        {
+          path: "play-featured-video",
+          element: <PlayFeaturedVideo />,
+        },
+      ],
     },
   ]);
 
