7a7e1e82ecaef0998c196801c5dc43df45836600
haoltse182646 <haoltse182646@fpt.edu.vn>
2025-02-25 16:20:07 +0700

    CommunityPost

--- SchoolTV_FE_Source/src/pages/CommunityPost/CommunityPost.jsx
+++ SchoolTV_FE_Source/src/pages/CommunityPost/CommunityPost.jsx
@@ -1,33 +1,120 @@
 import './CommunityPost.scss';
-import { Avatar, Form, Space } from 'antd';
-import { AntDesignOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Breadcrumb, Form, Input, Tooltip } from 'antd';
+import { AntDesignOutlined, FireOutlined, GlobalOutlined, HeartOutlined, MessageOutlined, SaveOutlined, SearchOutlined, SendOutlined, SmileOutlined, UserOutlined } from '@ant-design/icons';
+import FPTU from '../../img/FPTU.jpg';
+import Carousel from '../../components/carousel/carousel';
 
 function CommunityPost() {
+
   return (
     <div className="community-post-container">
-      <div className="post-home">
-      <a href="/Home">Home</a> 
-      <span></span>
-      </div>
       <div>
+        <Carousel />
+      </div>
+
+      <div className="post-home">
+        <Breadcrumb
+          separator=">"
+          items={[
+            {
+              title: 'Home',
+              href: '/watchHome'
+            },
+            {
+              title: 'Bài Viết Cộng Đồng',
+              href: '/CommunityPost',
+            },
+          ]}
+        />
+      </div>
+
+      <div className="post-form">
         <Form>
             <div className="post-avatar">
-            <Space wrap size={16}>
-            <Avatar
-              size={{
-                xs: 16,
-                sm: 20,
-                md: 24,
-                lg: 32,
-                xl: 64,
-                xxl: 80,
-              }}
-              icon={<AntDesignOutlined />}
-            />
-            </Space>
+              <Avatar size={50} icon={<UserOutlined />} />
+                <div className="post-info">
+                  <a className="username">FPT University HCM</a>
+                  <a className="time">2 hours ago <GlobalOutlined/></a>
+                </div>
+            </div>
+
+            <div className="post-content">
+              <span>
+                <FireOutlined /> Sau một tuần trở lại trường, FPTUers đã vào mood chạy deadline chưaa!!?
+              </span>
+              <span>
+                Điểm danh những đồng chí cuối tuần vẫn miệt mài teamworks - taowork nào?
+              </span>
+              <span className="hashtags">
+                #TruongDaiHocFPT #FPTU #FPTUniversity #FPTUniversityLife #FPTFutureYou
+              </span>
+            </div>
+
+            <div className="post-image">
+              <img src={FPTU} alt="" />
+            </div>
+
+            <div className="post-like">
+              <HeartOutlined />
+              <MessageOutlined />
+              <SendOutlined />
+              <SaveOutlined className="post-save"/>
+            </div>
+
+            <div className="post-cmt-container">
+            <Avatar.Group
+                size="medium"
+                max={{
+                  count: 2,
+                  style: {
+                    color: '#f56a00',
+                    backgroundColor: '#fde3cf',
+                  },
+                }}
+              >
+                <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=3" />
+                <Avatar
+                  style={{
+                    backgroundColor: '#f56a00',
+                  }}
+                >
+                  K
+                </Avatar>
+                <Tooltip title="Ant User" placement="top">
+                  <Avatar
+                    style={{
+                      backgroundColor: '#87d068',
+                    }}
+                    icon={<UserOutlined />}
+                  />
+                </Tooltip>
+                <Avatar
+                  style={{
+                    backgroundColor: '#1677ff',
+                  }}
+                  icon={<AntDesignOutlined />}
+                />
+              </Avatar.Group>
+
+              <span>Liked by <strong>phamtuananh</strong> and others</span>
+            </div>
+
+            <div className="post-view-cmt">
+              <span>View all 22,300 comments</span>
+            </div>      
+
+            <div className="post-text-area">
+              <Input 
+                placeholder="Add a comment" 
+                addonAfter={<SmileOutlined />}
+              />
             </div>
         </Form>
-        </div>
+      </div>
+
+      <div className="post-view-more">
+          <a>View more</a>
+      </div>
     </div>
   )
 }
